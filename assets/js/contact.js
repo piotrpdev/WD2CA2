0dd7e038687d2c13e45d6dd048492f622fa25d92
piotrpdev <peterplaczek03@gmail.com>
2022-03-31 13:20:05 +0100

    Captcha should be working

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,52 @@
+function autoFill() {
+  $('#contactname').val('John Doe');
+  $('#contactemail').val('john@doe.ie')
+}
+
 $(document).ready(() => {
     $(".dropdown").dropdown();
     $('textarea#details').richText();
 
-    // const wordCount = $('.richText-editor')[0].innerText.replace(/\n+/g, ' ').replace(' ', ' ').trim().split(' ').length
+    autoFill();
+
+    const rand1 = Math.floor(Math.random() * 10) + 1;
+    const rand2 = Math.floor(Math.random() * 10) + 1;
+
+    $('[for="captcha"]').text(`Captcha (${rand1} + ${rand2})`);
+
+    $('.richText-editor').keyup(() => {
+      const words = $('.richText-editor')[0].innerText.replace(/\n+/g, ' ').replace(' ', ' ').trim();
+      const wordCount = words === '' ? 0 : words.split(' ').length;
+      $('[for="details"]').text(`Details (${wordCount} words)`);
+    })
+
+    let listenerSet = false;
 
     $("form").submit((e) => {
       e.preventDefault();
-      $(".ui.basic.modal")
-        .modal("setting", "transition", "fade")
-        .modal({
-          onHide: function () {
-            window.location = "index.html";
-          },
-        })
-        .modal("show");
+
+      console.log(`${rand1 + rand2}`);
+      if ($('#captcha').val() === `${rand1 + rand2}`) {
+        $(".ui.basic.modal")
+          .modal("setting", "transition", "fade")
+          .modal({
+            onHide: function () {
+              window.location = "index.html";
+            },
+          })
+          .modal("show");
+      } else {
+        $('#captcha')[0].setCustomValidity('Incorrect answer!');
+        $('#captcha')[0].reportValidity();
+
+        if (!listenerSet) {
+          $('#captcha').on('change', () => {
+            $('#captcha')[0].setCustomValidity('');
+            $('#captcha')[0].reportValidity();
+          });
+          
+          listenerSet = true;
+        }
+      }
     });
   });
